89dc99d778490bc721db25802faf58b632c260b4
JustinSHong <jhong2009@gmail.com>
2019-06-01 17:42:30 -0400

    define log middleware

--- src/server.js
+++ src/server.js
@@ -5,6 +5,11 @@ import cors from 'cors'
 
 export const app = express()
 
+const log = (req, res, next) => {
+  console.log('logging')
+  req.logData = 'this is a log'
+  next() // accepts an argument that is an error object
+}
 app.disable('x-powered-by')
 
 // middleware to transform request
@@ -12,6 +17,7 @@ app.use(cors()) // enables CORS for server - allow interactions among domains
 app.use(json()) // returns data as JS object
 app.use(urlencoded({ extended: true })) // attach params to a url
 app.use(morgan('dev')) // logging functionality
+app.use(log)
 
 app.get('/', (req, res) => {
   res.send({ message: 'hello' })
@@ -26,8 +32,10 @@ app.get('/helloWorld', (req, res) => {
   res.send({ message: 'the world says hello friend' })
 })
 
-app.get('/data', (req, res) => {
-  res.send({ message: 'hello' })
+// run log middleware for /data route
+app.get('/data', log, (req, res) => {
+  //   res.send({ message: 'hello' })
+  res.send({ message: req.logData })
 })
 
 app.post('/data', (req, res) => {
