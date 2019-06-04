d0de6e193440d9baa061b59e2cfc8f4cfc892c65
JustinSHong <jhong2009@gmail.com>
2019-06-03 23:48:15 -0400

    item.model: set up model for item resource

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,44 @@
 import mongoose from 'mongoose'
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const { ObjectId } = mongoose.Schema.Types
+
+// schema options
+const options = {
+  timestamps: true
+}
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 50
+    },
+    status: {
+      type: String,
+      required: true,
+      default: 'active',
+      enum: ['active', 'complete', 'pastdue']
+    },
+    notes: String,
+    due: Date,
+    createdBy: {
+      type: ObjectId,
+      required: true,
+      ref: 'user' // refer to the user model
+    },
+    list: {
+      type: ObjectId,
+      required: true,
+      ref: 'list' // refer to the list model
+    }
+  },
+  options
+)
+
+// for a list, make sure the name of the task is unique
+// order of the fields matters since it dictates traversal
+itemSchema.index({ list: 1, name: 1 }, { unique: true })
+
 export const Item = mongoose.model('item', itemSchema)
