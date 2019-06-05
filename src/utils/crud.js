b38bdf575f420b1ef3f32c3f533f455a24379be4
JustinSHong <jhong2009@gmail.com>
2019-06-04 20:55:50 -0400

    create getOne controller

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,4 +1,17 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
+
+  try {
+    const query = await model.findOne({ _id: id, createdBy: userId }).exec()
+
+    if (!query) return res.status(400).end()
+
+    res.status(200).json({ data: query })
+  } catch (err) {
+    res.status(400).end()
+  }
+}
 
 export const getMany = model => async (req, res) => {}
 
