42e485023e501202bee077f5ef03bebf37f0fc34
JustinSHong <jhong2009@gmail.com>
2019-06-04 21:55:37 -0400

    add try catch blocks

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -42,28 +42,35 @@ export const updateOne = model => async (req, res) => {
   const id = req.params.id
   const body = req.body
 
-  const query = await model.findOneAndUpdate(
-    { _id: id, createdBy: userId },
-    body,
-    { new: true }
-  )
+  try {
+    const query = await model.findOneAndUpdate(
+      { _id: id, createdBy: userId },
+      body,
+      { new: true }
+    )
 
-  if (!query) return res.status(400).end()
-
-  res.status(200).json({ data: query })
+    if (!query) return res.status(400).end()
+    res.status(200).json({ data: query })
+  } catch (err) {
+    res.status(400).end()
+  }
 }
 
 export const removeOne = model => async (req, res) => {
   const userId = req.user._id
   const id = req.params.id
 
-  const query = await model
-    .findOneAndRemove({ _id: id, createdBy: userId })
-    .exec()
+  try {
+    const query = await model
+      .findOneAndRemove({ _id: id, createdBy: userId })
+      .exec()
 
-  if (!query) return res.status(400).end()
+    if (!query) return res.status(400).end()
 
-  res.status(200).json({ data: query })
+    res.status(200).json({ data: query })
+  } catch (err) {
+    res.status(400).end()
+  }
 }
 
 export const crudControllers = model => ({
